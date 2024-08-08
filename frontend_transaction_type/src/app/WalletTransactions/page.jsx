78c4a5c0c4dc4a18a6958018f0ddabe4f27f2247
Ganesh-Mirashi <ganesh.mirashi@ascenddefi.com>
2024-08-08 12:19:37 +0530

    created frontend structure and design

---
+++ frontend_transaction_type/src/app/WalletTransactions/page.jsx
@@ -0,0 +1,17 @@
+// src/SignInGoogle/Home.js
+'use client';
+
+import React from 'react'
+import UserForm from './WalletTransactions'
+import CurrencyForm from './WalletTransactions';
+
+
+function App() {
+  return (
+    <div>
+      <CurrencyForm />
+    </div>
+  );
+}
+
+export default App
